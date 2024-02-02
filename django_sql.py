b32fe008f1e3d3f6e3535f80b50a49b6bab77cd8
ErikFantomex <a217213318@unison.mx>
2024-02-01 18:24:50 -0700

    feat: enteder el ORM de django

--- django_sql.py
+++ django_sql.py
@@ -1,9 +1,4 @@
 #Python django:-
-#Ejemplos de como manipular bases de datos en django 
-from .models import World
-from django.db.models import Q
-data = World.objects.filter(Q(area__gte=3000000) | Q(population__gte=25000000))
-Table: Views
 +---------------+---------+
 | Column Name   | Type    |
 +---------------+---------+
@@ -44,3 +39,98 @@ Output:
 from .models import Views
 from django.db.models import F
 data = Views.objects.filter(author_id=F('viewer_id')).values('author_id').distinct().order_by('author_id')
+
+#Problema 2 
+Table: Tweets
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+tweet_id is the primary key (column with unique values) for this table.
+This table contains all the tweets in a social media app.
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Return the result table in any order.
+
+The result format is in the following example.
+Example 1:
+
+Input: 
+Tweets table:
++----------+----------------------------------+
+| tweet_id | content                          |
++----------+----------------------------------+
+| 1        | Vote for Biden                   |
+| 2        | Let us make America great again! |
++----------+----------------------------------+
+Output: 
++----------+
+| tweet_id |
++----------+
+| 2        |
++----------+
+Explanation: 
+Tweet 1 has length = 14. It is a valid tweet.
+Tweet 2 has length = 32. It is an invalid tweet.
+
+ from .models import Tweets
+ from django.db.models import F, Func, IntegerField
+
+ class Length(Func):
+     function = 'LENGTH'
+     output_field = IntegerField()
+ data = Tweets.objects.annotate(content_length=Length(F('content'))).filter(content_length__gt=15).values('tweet_id')
+
+
+
+
+# Python Django:
+Table: Employees
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+Table: EmployeeUNI
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+Return the result table in any order.
+The result format is in the following example.
+# models.py:-
+ class EmployeeUNI(models.Model):
+     id = models.IntegerField(primary_key=True)
+     unique_id = models.IntegerField()
+ class Employees(models.Model):
+     id = models.IntegerField(primary_key=True)
+     name = models.CharField(max_length=50)    
+ #views.py:
+ from django.shortcuts import render
+ from .models import Employees, EmployeeUNI
+ from django.db.models import OuterRef, Subquery
+ # Create your views here.
+ def index(request):
+     subquery = EmployeeUNI.objects.filter(id=OuterRef('id')).values('unique_id')[:1]
+
+     result = Employees.objects.annotate(unique_id=Subquery(subquery)).values('id', 'unique_id', 'name')
+     print(result)
+     return render(request, 'home.html', {'data':result})
+
+
+
+
+
+
